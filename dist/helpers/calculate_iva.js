52d33ed9c724b9fc7dd776e5556b3b8925921789
Frederico Andrade <100705644+Fred-And@users.noreply.github.com>
2025-04-08 18:12:58 +0100

    Delete dist directory

--- dist/helpers/calculate_iva.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateIva = void 0;
-const calculateIva = (value) => {
-    const ivaRate = 0.23; // 23% IVA rate
-    return value * (1 + ivaRate);
-};
-exports.calculateIva = calculateIva;
